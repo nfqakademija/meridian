c76bfc1da5e60e30ced0a30000e6902f94ec758a
Mantas Jonusas <chyzas@gmail.com>
2014-11-23 10:24:04 +0200

    idetas i admina nauju parodu redagavimas

---
+++ src/Meridian/CoreBundle/Form/ExpoType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Meridian\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ExpoType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('time')
+            ->add('link')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Meridian\CoreBundle\Entity\Expo'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'meridian_corebundle_expo';
+    }
+}
