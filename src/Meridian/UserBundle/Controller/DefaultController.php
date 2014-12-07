6ee7ab49f6aab279cf38230337dbb272aa42c9d1
Mantas Jonusas <chyzas@gmail.com>
2014-12-07 19:04:26 +0200

    fixed login bugs

--- src/Meridian/UserBundle/Controller/DefaultController.php
+++ src/Meridian/UserBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\ExpressionLanguage\Token;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\SecurityContext;
 
 
 class DefaultController extends Controller
@@ -15,14 +16,35 @@ class DefaultController extends Controller
         return $this->render('MeridianUserBundle:Default:welcome.html.twig');
     }
 
-    public function startAction()
+    public function startAction(Request $request)
     {
         $user = $this->get('security.context')->getToken()->getUser();
         if ($user != 'anon.'){
             return $this->redirect($this->generateUrl("welcome_page"));
         }
-        return $this->render('MeridianUserBundle:Default:start.html.twig');
+
+        /** @var $session \Symfony\Component\HttpFoundation\Session\Session */
+        $session = $request->getSession();
+
+        // get the error if any
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        if ($error) {
+            $error = $error->getMessage();
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+            $session->save();
+        }
+
+        return $this->render('MeridianUserBundle:Default:start.html.twig', ['error' => $error]);
     }
+
+
 
     public function aboutAction()
     {
